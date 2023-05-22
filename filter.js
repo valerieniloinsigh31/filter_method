dba9dee320addae59e43569a4cde941301f8695d
valerieniloinsigh31 <valerieniloinsigh25@gmail.com>
2023-05-22 16:12:22 +0000

    Filter method for Advanced JS Complete

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(person => person.name === "Paul")[0];
+console.log(paul);
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,8 @@ const students = [
     ]
   },
 ];
+
+const has5YearsExp = skill => skill.yrsExperience >= 5; 
+const hasStrongSkills = student => student.skills.filter(has5YearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
